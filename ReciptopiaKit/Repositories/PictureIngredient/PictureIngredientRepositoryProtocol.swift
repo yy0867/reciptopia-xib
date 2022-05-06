24d1bfde8c38e4bf0b43a8f000b34cfe79d21530
김세영 <yy0867@gmail.com>
2022-05-06 10:13:15 +0900

    add: PictureIngredientRepositoryProtocol 추가

---
+++ ReciptopiaKit/Repositories/PictureIngredient/PictureIngredientRepositoryProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  PictureIngredientRepositoryProtocol.swift
+//  ReciptopiaKit
+//
+//  Created by 김세영 on 2022/05/06.
+//
+
+import Foundation
+
+internal protocol PictureIngredientRepositoryProtocol {
+    
+}
