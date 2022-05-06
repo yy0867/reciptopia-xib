362888ce385ba1c3234aa7267cf79de81bdf30e6
김세영 <yy0867@gmail.com>
2022-05-06 14:41:45 +0900

    add: register, resolve 메서드 추가

--- Reciptopia/Infrastructure/DependencyContainer/DependencyContainer.swift
+++ Reciptopia/Infrastructure/DependencyContainer/DependencyContainer.swift
@@ -12,4 +12,25 @@ class DependencyContainer {
     static let shared = DependencyContainer()
     private var dependencies = [String: Any]()
     private init() {}
+    
+    private func makeKey<T>(of type: T.Type) -> String {
+        return String(describing: type.self)
+    }
+    
+    func register<T>(_ type: T.Type, _ instance: (DependencyContainer) -> T) {
+        let key = makeKey(of: type)
+        let instance = instance(DependencyContainer.shared)
+        
+        dependencies.updateValue(instance, forKey: key)
+    }
+    
+    func resolve<T>() -> T {
+        let key = makeKey(of: T.self)
+        
+        guard let instance = dependencies[key] as? T else {
+            fatalError("Fail to get \(key)'s instance.")
+        }
+        
+        return instance
+    }
 }
