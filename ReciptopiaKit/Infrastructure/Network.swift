a0b48e3eb575b980b6682a27437f8f55d43b1d60
김세영 <yy0867@gmail.com>
2022-05-06 09:24:56 +0900

    add: Network 클래스 추가

---
+++ ReciptopiaKit/Infrastructure/Network.swift
@@ -0,0 +1,14 @@
+//
+//  Network.swift
+//  ReciptopiaKit
+//
+//  Created by 김세영 on 2022/05/06.
+//
+
+import Foundation
+
+internal class Network {
+    
+    let shared = Network()
+    private init() {}
+}
