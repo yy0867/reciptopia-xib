fb3cc0fb26e2194b82487017e45e9c82329d579a
김세영 <yy0867@gmail.com>
2022-05-06 10:46:29 +0900

    add: Ingredient 모델 추가

---
+++ ReciptopiaKit/Models/Ingredient.swift
@@ -0,0 +1,12 @@
+//
+//  Ingredient.swift
+//  ReciptopiaKit
+//
+//  Created by 김세영 on 2022/05/06.
+//
+
+public struct Ingredient: Codable {
+    public let id: Int?
+    public let name: String
+    public let detail: String
+}
