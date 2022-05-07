29dc240d2002b7a93493acfbfa7088aa08b96895
김세영 <yy0867@gmail.com>
2022-05-07 10:42:08 +0900

    add: KeychainTokenDataStore 클래스 생성 및 TokenDataStoreProtocol 채택

---
+++ Reciptopia/DataStores/UserSession/KeychainTokenDataStore.swift
@@ -0,0 +1,26 @@
+//
+//  KeychainTokenDataStore.swift
+//  Reciptopia
+//
+//  Created by 김세영 on 2022/05/07.
+//
+
+import Foundation
+
+final class KeychainTokenDataStore: TokenDataStoreProtocol {
+    
+    // MARK: - Properties
+    
+    // MARK: - Methods
+    func addToken(_ token: String) -> Bool {
+        
+    }
+    
+    func searchToken() -> String? {
+        
+    }
+    
+    func deleteToken() -> Bool {
+        
+    }
+}
