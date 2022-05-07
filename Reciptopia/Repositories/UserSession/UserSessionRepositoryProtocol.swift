0997e86f2cff323798e9e743e64ffae9d1060c5d
김세영 <yy0867@gmail.com>
2022-05-07 13:36:03 +0900

    add: UserSessionRepositoryProtocol 생성

---
+++ Reciptopia/Repositories/UserSession/UserSessionRepositoryProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  UserSessionRepositoryProtocol.swift
+//  Reciptopia
+//
+//  Created by 김세영 on 2022/05/07.
+//
+
+import Foundation
+import RxSwift
+
+protocol UserSessionRepositoryProtocol {
+    func signIn(credential: Credential) -> Observable<UserSession>
+    func signUp(credential: Credential, nickname: String) -> Observable<Account>
+    func readUserSession() -> Observable<UserSession>
+    func signOut() -> Observable<Void>
+    func isExists(email: String) -> Observable<Exist>
+    func withdrawal() -> Observable<Void>
+}
