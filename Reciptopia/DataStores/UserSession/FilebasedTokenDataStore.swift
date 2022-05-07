0668f293742534f721f3ceb468d4445ac16c7122
김세영 <yy0867@gmail.com>
2022-05-07 13:18:30 +0900

    add: FilebasedTokenDataStore 클래스 생성
    디버그 시 토큰을 저장할, FileManager을 사용한 토큰 저장소

---
+++ Reciptopia/DataStores/UserSession/FilebasedTokenDataStore.swift
@@ -0,0 +1,15 @@
+//
+//  FilebasedTokenDataStore.swift
+//  Reciptopia
+//
+//  Created by 김세영 on 2022/05/07.
+//
+
+import Foundation
+
+final class FilebasedTokenDataStore: TokenDataStoreProtocol {
+    
+    // MARK: - Properties
+    
+    // MARK: - Methods
+}
