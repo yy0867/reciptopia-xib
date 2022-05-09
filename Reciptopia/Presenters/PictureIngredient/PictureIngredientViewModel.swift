61edf9303994755840788579dce5b6dddc7b3af8
김세영 <yy0867@gmail.com>
2022-05-09 23:49:53 +0900

    add: #47feee에 따라 PictureIngredientViewModel 구현

--- Reciptopia/Presenters/PictureIngredient/PictureIngredientViewModel.swift
+++ Reciptopia/Presenters/PictureIngredient/PictureIngredientViewModel.swift
@@ -7,4 +7,28 @@
 
 import Foundation
 import RxSwift
-import RxCocoa
+import RxRelay
+
+final class PictureIngredientViewModel {
+    
+    // MARK: - Properties
+    private let useCase: AnalyzePictureUseCaseProtocol
+    
+    let pictures = BehaviorRelay<[Data]>(value: [])
+    let maxPictureCount = 10
+    
+    // MARK: - Methods
+    init(useCase: AnalyzePictureUseCaseProtocol) {
+        self.useCase = useCase
+    }
+    
+    func addPicture(_ data: Data) {
+        if pictures.value.count >= 10 { return }
+        pictures.append(data)
+    }
+    
+    func removePicture(at index: Int) {
+        guard 0 <= index && index < pictures.value.count else { return }
+        pictures.remove(at: index)
+    }
+}
