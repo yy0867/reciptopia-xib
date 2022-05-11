5d1b31e3ebb9511510c073b0d09a856162cc280c
김세영 <yy0867@gmail.com>
2022-05-11 09:07:39 +0900

    add: Analyze 관련 메서드 추가, AnalyzeState 추가

--- Reciptopia/Presenters/PictureIngredient/PictureIngredientViewModel.swift
+++ Reciptopia/Presenters/PictureIngredient/PictureIngredientViewModel.swift
@@ -11,10 +11,18 @@ import RxRelay
 
 final class PictureIngredientViewModel {
     
+    enum AnalyzeState {
+        case analyzing
+        case errorDetected
+        case analyzed(analyzedResult: [Ingredient])
+    }
+    
     // MARK: - Properties
     private let useCase: AnalyzePictureUseCaseProtocol
+    private var subscription: Disposable?
     
     let pictures = BehaviorRelay<[Data]>(value: [])
+    let analyzeState = PublishRelay<AnalyzeState>()
     let maxPictureCount = 10
     
     // MARK: - Methods
@@ -30,5 +38,24 @@ final class PictureIngredientViewModel {
     func removePicture(at index: Int) {
         guard 0 <= index && index < pictures.value.count else { return }
         pictures.remove(at: index)
+    }
+    
+    func analyze() {
+        self.analyzeState.accept(.analyzing)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.subscription = self.useCase.execute(self.pictures.value)
+                .subscribe(onNext: self.analyzed, onError: self.errorDetected, onDisposed: { [weak self] in
+                    self?.subscription?.dispose()
+                })
+        }
+    }
+    
+    // MARK: - Private
+    private func analyzed(with ingredients: [Ingredient]) {
+        analyzeState.accept(.analyzed(analyzedResult: ingredients))
+    }
+    
+    private func errorDetected(_ error: Error) {
+        analyzeState.accept(.errorDetected)
     }
 }
