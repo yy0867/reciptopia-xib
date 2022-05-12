f4db5f8f9f93fe1d1af156efd90922dad9df0d11
김세영 <yy0867@gmail.com>
2022-05-12 11:21:07 +0900

    add: SignedInMyPageViewController 추가 및 스토리보드 작성

---
+++ Reciptopia/Presenters+Dependencies/MyPage/SignedIn/SignedInMyPageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignedInMyPageViewController.swift
+//  Reciptopia
+//
+//  Created by 김세영 on 2022/05/12.
+//
+
+import UIKit
+
+class SignedInMyPageViewController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
