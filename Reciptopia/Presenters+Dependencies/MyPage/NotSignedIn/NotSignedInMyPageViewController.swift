2ccd16cf0a4b9bbb5ac68eff3d69d37aa0830c3b
김세영 <yy0867@gmail.com>
2022-05-12 11:00:06 +0900

    add: NotSignedInViewController 추가 및 스토리보드 작성

---
+++ Reciptopia/Presenters+Dependencies/MyPage/NotSignedIn/NotSignedInMyPageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NotSignedInMyPageViewController.swift
+//  Reciptopia
+//
+//  Created by 김세영 on 2022/05/12.
+//
+
+import UIKit
+
+class NotSignedInMyPageViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
