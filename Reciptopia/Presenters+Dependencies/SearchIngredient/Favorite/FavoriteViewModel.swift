2a6755fe69de43b4210d995478d236d8cc2b67d1
김세영 <yy0867@gmail.com>
2022-05-15 10:58:35 +0900

    add: FavoriteViewModel 클래스 추가

---
+++ Reciptopia/Presenters+Dependencies/SearchIngredient/Favorite/FavoriteViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteViewModel.swift
+//  Reciptopia
+//
+//  Created by 김세영 on 2022/05/15.
+//
+
+import Foundation
+import RxRelay
+
+final class FavoriteViewModel {
+    
+    // MARK: - Properties
+    
+    // MARK: - Methods
+}
