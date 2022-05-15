ae40906c40b65cb0c4398c6633f75bfd37346af5
김세영 <yy0867@gmail.com>
2022-05-15 11:04:29 +0900

    add: fetch, save, delete 메서드 추가

--- Reciptopia/Presenters+Dependencies/SearchIngredient/Favorite/FavoriteViewModel.swift
+++ Reciptopia/Presenters+Dependencies/SearchIngredient/Favorite/FavoriteViewModel.swift
@@ -7,10 +7,30 @@
 
 import Foundation
 import RxRelay
+import RxSwift
 
 final class FavoriteViewModel {
     
     // MARK: - Properties
+    private let repository: FavoriteRepositoryProtocol
+    private var subscription: Disposable?
+    
+    let favorites = BehaviorRelay<[Favorite]>(value: [])
     
     // MARK: - Methods
+    init(repository: FavoriteRepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    func fetch() {
+        
+    }
+    
+    func save(post: Post) {
+        
+    }
+    
+    func delete(favorite: Favorite) {
+        
+    }
 }
