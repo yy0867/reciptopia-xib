50e22861769baa68f1cdc80bb084b4e6902bce14
김세영 <yy0867@gmail.com>
2022-05-16 20:18:44 +0900

    add: SearchHistory 스토리보드 및 클래스 추가

---
+++ Reciptopia/Presenters+Dependencies/SearchIngredient/SearchHistory/SearchHistoryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchHistoryViewController.swift
+//  Reciptopia
+//
+//  Created by 김세영 on 2022/05/16.
+//
+
+import UIKit
+
+class SearchHistoryViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
