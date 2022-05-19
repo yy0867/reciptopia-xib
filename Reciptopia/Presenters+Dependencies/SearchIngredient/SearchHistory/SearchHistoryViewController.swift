13de216cf69d23ace5f7e350decd146542c94892
김세영 <yy0867@gmail.com>
2022-05-19 13:54:29 +0900

    add: SearchHistoryTableView와 ViewModel 바인딩

--- Reciptopia/Presenters+Dependencies/SearchIngredient/SearchHistory/SearchHistoryViewController.swift
+++ Reciptopia/Presenters+Dependencies/SearchIngredient/SearchHistory/SearchHistoryViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class SearchHistoryViewController: UIViewController {
+class SearchHistoryViewController: UIViewController, StoryboardInstantiable {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var searchHistoryTableView: UITableView!
     
     // MARK: - Properties
+    private var viewModel: SearchHistoryViewModel!
+    private let disposeBag = DisposeBag()
     
     // MARK: - Methods
+    static func create(viewModel: SearchHistoryViewModel) -> Self {
+        let vc = self.instantiateViewController()
+        vc.viewModel = viewModel
+        return vc
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        bindViewModel()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.fetch()
+    }
+}
+
+// MARK: - Bind ViewModel
+extension SearchHistoryViewController {
+    private func bindViewModel() {
+        bindSearchHistoriesToTableView()
+    }
+    
+    private func bindSearchHistoriesToTableView() {
+        viewModel.histories
+            .bind(
+                to: searchHistoryTableView.rx.items(
+                    cellIdentifier: SearchHistoryCell.reuseIdentifier,
+                    cellType: SearchHistoryCell.self
+                )
+            )(bindSearchHistoryToTableViewCell)
+            .disposed(by: disposeBag)
+    }
+    
+    private func bindSearchHistoryToTableViewCell(
+        index: Int,
+        history: SearchHistory,
+        cell: SearchHistoryCell
+    ) {
+        cell.configureCell(history)
+    }
+}
+
+// MARK: - UITableView Delegate
+extension SearchHistoryViewController: UITableViewDelegate {
 }
