46f86e42ed1f176dadebdf0185e856b90355145b
김세영 <yy0867@gmail.com>
2022-05-20 16:05:15 +0900

    add: FavoriteCell 클래스 및 xib 추가

---
+++ Reciptopia/Presenters+Dependencies/SearchIngredient/Favorite/FavoriteCell/FavoriteCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteCell.swift
+//  Reciptopia
+//
+//  Created by 김세영 on 2022/05/20.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(false, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
