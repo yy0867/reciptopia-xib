c2af0306e059059dd2664b9fc1179f1fa9043145
김세영 <yy0867@gmail.com>
2022-05-21 00:44:00 +0900

    add: SearchIngredientViewModel 구현

--- Reciptopia/Presenters+Dependencies/SearchIngredient/SearchIngredientViewModel.swift
+++ Reciptopia/Presenters+Dependencies/SearchIngredient/SearchIngredientViewModel.swift
@@ -6,5 +6,40 @@
 //
 
 import Foundation
+import RxRelay
 
-
+final class SearchIngredientViewModel {
+    
+    // MARK: - Properties
+    let ingredients = BehaviorRelay<[Ingredient]>(value: [])
+    let maxCount: Int = 10
+    
+    // MARK: - Methods
+    func addIngredient(_ ingredientName: String) {
+        guard ingredients.value.count < 10,
+              !ingredientName.isEmpty else { return }
+        
+        ingredients.append(Ingredient(name: ingredientName))
+    }
+    
+    func setIngredients(_ ingredients: [Ingredient]) {
+        var mutableIngredients = ingredients
+        if ingredients.count > maxCount {
+            mutableIngredients = Array(ingredients[0..<maxCount])
+        }
+        self.ingredients.accept(mutableIngredients)
+    }
+    
+    func toggleState(at index: Int) {
+        guard 0 <= index && index < ingredients.value.count else { return }
+        var mutableIngredients = ingredients.value
+        let isMainIngredient = ingredients.value[index].isMainIngredient
+        mutableIngredients[index].isMainIngredient = !isMainIngredient
+        ingredients.accept(mutableIngredients)
+    }
+    
+    func removeIngredient(at index: Int) {
+        guard 0 <= index && index < ingredients.value.count else { return }
+        ingredients.remove(at: index)
+    }
+}
