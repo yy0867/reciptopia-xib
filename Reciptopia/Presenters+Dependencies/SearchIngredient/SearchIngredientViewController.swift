5c0afdb5b038cdb0a1847dbf7dc12ff1c7a2759a
김세영 <yy0867@gmail.com>
2022-05-20 16:02:06 +0900

    add: SearchHistoryTableView와 FavoriteTableView 등록 + isHidden 바인딩 및 SearchHistoryViewModel 바인딩

--- Reciptopia/Presenters+Dependencies/SearchIngredient/SearchIngredientViewController.swift
+++ Reciptopia/Presenters+Dependencies/SearchIngredient/SearchIngredientViewController.swift
@@ -6,19 +6,80 @@
 //
 
 import UIKit
+import RxRelay
+import RxSwift
+import RxCocoa
 
-class SearchIngredientViewController: UIViewController {
+class SearchIngredientViewController: UIViewController, StoryboardInstantiable {
+    
+    enum Page {
+        case searchHistory
+        case favorite
+    }
     
     // MARK: - Outlets
     @IBOutlet weak var ingredientSearchBar: UISearchBar!
+    @IBOutlet weak var pageSegment: UISegmentedControl!
+    @IBOutlet weak var searchHistoryTableView: UITableView!
+    @IBOutlet weak var favoriteTableView: UITableView!
     
     // MARK: - Properties
+    private var searchHistoryViewModel: SearchHistoryViewModel!
+    private var favoriteViewModel: FavoriteViewModel!
     
+    let selectedPage = BehaviorRelay<Page>(value: .searchHistory)
+    private let disposeBag = DisposeBag()
     
     // MARK: - Methods
+    static func create(
+        searchHistoryViewModel: SearchHistoryViewModel,
+        favoriteViewModel: FavoriteViewModel
+    ) -> Self {
+        let vc = self.instantiateViewController()
+        vc.searchHistoryViewModel = searchHistoryViewModel
+        vc.favoriteViewModel = favoriteViewModel
+        return vc
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        bindSelectedPageToTableView()
+    }
+}
+
+// MARK: - Bind Page State
+extension SearchIngredientViewController {
+    private func bindSelectedPageToTableView() {
+        selectedPage
+            .map { $0 == .searchHistory }
+            .bind(
+                to: searchHistoryTableView.rx.isShown,
+                favoriteTableView.rx.isHidden
+            )
+            .disposed(by: disposeBag)
+    }
+}
+
+// MARK: - Bind ViewModel
+extension SearchIngredientViewController {
+    
+    // MARK: Search History
+    private func bindSearchHistoryTableView() {
+        searchHistoryViewModel.histories
+            .bind(to: searchHistoryTableView.rx.items(
+                    cellIdentifier: SearchHistoryCell.reuseIdentifier,
+                    cellType: SearchHistoryCell.self
+            ))(bindSearchHistoryCell)
+            .disposed(by: disposeBag)
+    }
+    
+    private func bindSearchHistoryCell(index: Int, item: SearchHistory, cell: SearchHistoryCell) {
+        cell.configureCell(item)
+    }
+    
+    // MARK: Favorite
+    private func bindFavoriteTableView() {
         
     }
 }
