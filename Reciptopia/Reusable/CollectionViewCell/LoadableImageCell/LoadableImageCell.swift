22e07452fdd2cc0aa888f51bd4b1b974df2c2fbf
김세영 <yy0867@gmail.com>
2022-05-24 17:35:47 +0900

    add: LoadableImageCell 추가

---
+++ Reciptopia/Reusable/CollectionViewCell/LoadableImageCell/LoadableImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  LoadableImageCell.swift
+//  Reciptopia
+//
+//  Created by 김세영 on 2022/05/24.
+//
+
+import UIKit
+
+class LoadableImageCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
